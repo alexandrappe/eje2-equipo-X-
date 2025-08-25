91fc505799d1bfb01d5cf9d0543db6deb1fddb67
MarcoGArana <00037822@uca.edu.sv>
2025-08-25 16:34:03 -0600

    rivorianos

--- actividadAldemir.pl
+++ actividadAldemir.pl
@@ -1,6 +1,7 @@
 esposo(esteban, mariela).
 esposo(andres, clara).
 esposo(rodolfo, teresa).
+
 hijo(clara, esteban).
 hijo(clara, mariela).
 hijo(rodolfo, esteban).
@@ -14,3 +15,8 @@ hijo(lucas, teresa).
 hijo(martina, rodolfo).
 hijo(martina, teresa).
 amigo(hector, ismael).
+
+decendiente(X,Y) :- hijo(X,Y).
+decendiente(X,Y) :- hijo(X,Z), decendiente(Z,Y).
+
+rivoriano(X) :- decendiente(X, esteban).
