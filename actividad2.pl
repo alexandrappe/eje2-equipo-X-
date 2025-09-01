aacee2f62cee6a99d2c40f85934936774eebe7e1
MarcoGArana <00037822@uca.edu.sv>
2025-09-01 16:33:28 -0600

    xd

--- actividad2.pl
+++ actividad2.pl
@@ -4,6 +4,24 @@ estudiante(florence).
 estudiante(mauricio).
 estudiante(melissa).
 
+:-\+ estudiante(ellacuria).
+
+primer_estudiante(E):-estudiante(E),!.
+
+mostrar_estudiantes :-
+    estudiante(Nombre),
+    write('Estudiante: '), write(Nombre), nl,
+    fail.
+mostrar_estudiantes.
+
+saludo_usuario :-
+    write('Ingrese su nombre: '), read(N),
+    (   estudiante(N)
+    ->  write('hola '), write(N) 
+    ;   write('quien chota sos?')
+    ).
+    fail.
+
 % Negacion: determinar si ellacuria no es estudiante (usando negacion) - solo consulta
 % Corte: obtener solo el primer estudiante de la lista - regla y consulta
 % Fallo: listar todos los estudiantes usando fail - una regla con condiciones (3 parametros y 1 opcional) y un paro
