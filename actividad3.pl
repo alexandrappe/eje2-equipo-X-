db20df9f2637dd77482c9e6fc85cdee59ae2e5fa
MarcoGArana <00037822@uca.edu.sv>
2025-09-03 16:44:29 -0600

    Add: 7 y 12

--- actividad3.pl
+++ actividad3.pl
@@ -1,4 +1,21 @@
+% 3. Operadores de Ligadura y Unificación
+% Ejercicio 7: Unificación de estructuras - Crear predicado es_hermano/2.
+hijo(clara, esteban).
+hijo(clara, mariela).
+hijo(rodolfo, esteban).
+hijo(rodolfo, mariela).
+hijo(ismael, esteban).
+hijo(ismael, mariela).
+hijo(sofia, clara).
+hijo(sofia, andres).    
+
+es_hermano(Persona1, Persona2):-
+    ((hijo(Persona1, Papa), hijo(Persona2, Papa)) -> Bros = 'Son hermanos' ; Bros = 'no son hermanos'),
+    write(Bros).
+
+%---------------------------------------
 % 4. Funciones Aritmeticas Predefinidas
+
 % 10- Distancia entre dos puntos
 distancia(X1,X2,Y1,Y2,D) :-
     D is sqrt((X2-X1)^2 + (Y2-Y1)^2).
@@ -6,6 +23,14 @@ distancia(X1,X2,Y1,Y2,D) :-
 % 11- Conversion de grados a radianes
 grados_a_radianes(G, R) :-
     R is G * (pi / 180).
+
+% 12: Promedios y extremos - Crear analisis/3.
+promedio_y_extremo(X, Y, Z):-
+    Promedio is (X + Y + Z) / 3,
+    Maximo is max(max(X, Y), Z),
+    Minimo is min(min(X, Y), Z),
+    write('Promedio: '), write(Promedio), nl,
+    write('Extremos: '), write(Minimo), write(', '), write(Maximo), nl.
 
 % 13- Estadisticas (abs, round, floor, ceil)
 % Valor absoluto
@@ -23,3 +48,5 @@ redondear_abajo(N, R) :-
 % Ceil
 redondear_arriba(N, R) :-
     R is ceil(N).
+
+    
