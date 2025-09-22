9f3b76b346ed71722986ca8617b4a23ed364f7fa
MarcoGArana <00037822@uca.edu.sv>
2025-09-22 15:59:39 -0600

    a

--- famiia.pl
+++ famiia.pl
@@ -1,3 +1,37 @@
+padre(ernesto, carlos).
+padre(ernesto, ana).
+padre(ernesto, lucia).
+padre(carlos,mateo).
+padre(carlos,valeria).
+padre(ricardo, daniel).
+padre(miguel, fernanda).
+padre(felipe, joaquin).
+
+madre(teresa, carlos).
+madre(teresa, ana).
+madre(teresa, lucia).
+madre(sofia, mateo).
+madre(sofia, valeria).
+madre(ana, andres).
+madre(laura, daniel).
+madre(rosa, fernanda).
+
+hijo(carlos, ernesto).
+hijo(carlos, teresa).
+hijo(ana, ernesto).
+hijo(ana, teresa).
+hijo(lucia, ernesto).
+hijo(lucia, teresa).
+hijo(mateo, carlos).
+hijo(mateo, sofia).
+hijo(valeria, carlos).
+hijo(valeria, sofia).
+hijo(daniel, ricardo).
+hijo(daniel, laura).
+hijo(fernanda, rosa).
+hijo(fernanda, miguel).
+hijo(joaquin, felipe).
+
 amigo(carlos,ricardo).
 amigo(daniel,mateo).
 amigo(laura,valeria).
