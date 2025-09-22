8523edd4a2dfc431c507153671081bb7a5cc510d
Melissaav22 <00135922@uca.edu.sv>
2025-09-22 16:07:08 -0600

    abuelo

--- famiia.pl
+++ famiia.pl
@@ -11,3 +11,10 @@ casado_con(ernesto, teresa).
 casado_con(carlos, sofia).
 casado_con(ricardo, laura).
 casado_con(miguel, rosa).
+
+
+
+
+% determinar si alguien es abuelo/abuela
+abuelo(Abuelo,Nieto) :- hijo(Nieto,Padre), hijo(Padre,Abuelo).
+abuela(Abuela,Nieto) :- hijo(Nieto,Padre), hijo(Padre,Abuela).
