978c3780b1ef366c0c8e17a6173da1bf03df621f
Melissaav22 <00135922@uca.edu.sv>
2025-09-22 15:55:56 -0600

    casados

--- famiia.pl
+++ famiia.pl
@@ -5,3 +5,9 @@ amigo(carmen,teresa).
 amigo(isabel, fernanda).
 amigo(felipe, miguel).
 amigo(joaquin, andres).
+
+% casados 
+casado_con(ernesto, teresa).
+casado_con(carlos, sofia).
+casado_con(ricardo, laura).
+casado_con(miguel, rosa).
