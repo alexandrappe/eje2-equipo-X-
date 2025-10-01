77eff03159b6820a7877e754a5b067e328cae91e
Melissaav22 <00135922@uca.edu.sv>
2025-10-01 16:16:32 -0600

    actividades

--- actividad4.pl
+++ actividad4.pl
@@ -6,3 +6,24 @@ amigo(maria, juan).
 amigo(juan,maria).
 amigo(pedro,sofia).
 amigo(sofia, pedro).
+
+
+% Actividades
+% pedro y sofia trabajan juntos en la tienda
+trabaja(pedro).
+trabaja(sofia).
+% maria y juan estudian en la biblioteca
+estudia(maria).
+estudia(juan).
+%ana cocina todos los dias
+cocina(ana).
+%menos cuando esta cansada
+descansa(ana).
+%juan descansa los fines de semana
+descansa(juan).
+%pedro juega los sabados
+juega(pedro).
+% sofia estudia por las noches
+estudia(sofia).
+%maria descansa los domingos
+descansa(maria).
